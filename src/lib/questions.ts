b6c7b6cbf70aed98b7fc55dc469547b47643e574
Neto Ukpong <nsilva1@users.noreply.github.com>
2025-07-26 12:53:27 +0100

    Update questions.ts
    New questions added to the questions array

--- src/lib/questions.ts
+++ src/lib/questions.ts
@@ -19,4 +19,63 @@ export const questions = [
         { answer: "Bad weather", points: 5 },
       ],
     },
+    {
+      question: "Name a room in a standard meetinghouse besides the Chapel",
+      answers: [
+        { answer: "Cultural Hall", points: 40 },
+        { answer: "Relief Society", points: 25 },
+        { answer: "Primary", points: 15 },
+        { answer: "Bishop", points: 12 },
+        { answer: "Clerk", points: 8 },
+      ],
+    },
+    {
+      question: "Name an item you would find in the pocket of a missionary's white shirt",
+      answers: [
+        { answer: "Pen", points: 38 },
+        { answer: "Planner", points: 26 },
+        { answer: "Pass-Along Card", points: 18 },
+        { answer: "Preach My Gospel", points: 10 },
+        { answer: "Lip Balm", points: 8 },
+      ],
+    },
+    {
+      question: "Name one of the four Standard Works.",
+      answers: [
+        { answer: "Book of Mormon", points: 45 },
+        { answer: "Bible", points: 30 },
+        { answer: "Doctrine & Covenants", points: 15 },
+        { answer: "Pearl of Great Price", points: 10 },
+      ],
+    },
+    {
+      question: "Name a major prophet from the Book of Mormon",
+      answers: [
+        { answer: "Nephi", points: 40 },
+        { answer: "Alma", points: 25 },
+        { answer: "Moroni", points: 20 },
+        { answer: "Lehi", points: 10 },
+        { answer: "King Benjamin", points: 5 },
+      ],
+    },
+    {
+      question: "Name a calling in a ward",
+      answers: [
+        { answer: "Bishop", points: 30 },
+        { answer: "Clerk", points: 22 },
+        { answer: "Teacher", points: 18 },
+        { answer: "Relief Society President", points: 9 },
+        { answer: "Organist / Pianist", points: 6 },
+      ],
+    },
+    {
+      question: "Name a food you would almost always expect to find at a ward potluck",
+      answers: [
+        { answer: "Jollof Rice", points: 35 },
+        { answer: "Egusi", points: 28 },
+        { answer: "Afang", points: 15 },
+        { answer: "Meatpie", points: 12 },
+        { answer: "Chicken", points: 10 },
+      ],
+    },
 ];
